77cc85f10c606e6602731daf8a034fb30c3f5724
Luiz Lopes <luizlopes12@outlook.com>
2022-01-18 09:32:13 -0300

    ambiente inicial

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
+import Header from './components/Header'
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Header/>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../styles/bootstrap.min.css'
+import '../styles/style.css'
+function Header() {
+    return (
+        <div className=''>
+            <div className="logo">
+                <img src='./../img/logo.svg'/>
+            </div>
+            <div className="nav">
+                <ul>
+                    <li>
+                        opa
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
