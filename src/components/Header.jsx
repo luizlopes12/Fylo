057777813e7f0ec52a9dbbd3f6d2dd7915fd005d
Luiz Lopes <luizlopes12@outlook.com>
2022-01-19 14:10:48 -0300

    react router dom

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Benefits from './Benefits'
+import {Route, BrowserRouter as Router, Routes, Link} from "react-router-dom";
 import '../styles/bootstrap.min.css'
 import '../styles/style.css'
 function Header() {
@@ -8,22 +10,42 @@ function Header() {
             <div className="logo col-sm-2 col-4 p-2 text-start">
                 <img src={require('../img/logo.svg').default} alt='Fylo logo' className='img-fluid'/>
             </div>
+            <Router>
             <nav className="navbar col-sm-4 col-8 d-flex justify-content-end">
                 <ul className='nav raleway'>
                     <li className="nav-item">
-                        <a href="#" className="nav-link">Features</a>
+                        <Link to='/' className="nav-link">Features</Link>
                     </li>
                     <li className="nav-item">
-                        <a href="#" className="nav-link">Team</a>
+                        <Link to='/' className="nav-link">Team</Link>
                     </li>
                     <li className="nav-item">
-                        <a href="#" className="nav-link">Sign In</a>
+                        <Link to='/' className="nav-link">Sign In</Link>
                     </li>
                 </ul>
             </nav>
+            <Routes>
+            {/* <Route path="/features" element={<Benefits />}></Route> */}
+            </Routes>
+            </Router>
+
             </div>
         </header>
     )
 }
+/*
+    <Router>
+    <div className="navbar">
+    <Link to='/'>Home</Link>
+    <Link to='/pag2'>Sobre</Link>
+    <Link to='/pag3'>Produtos</Link>
+    </div>
+    <Routes>
+    <Route path="/" element={<Pag1 />}></Route>
+    <Route path="/pag2" element={<Pag2 />}></Route>
+    <Route path="/pag3" element={<Pag3 />}></Route>
+    </Routes>
+    </Router>
 
+*/
 export default Header
